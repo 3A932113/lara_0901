9f278b2bc981d64b2206cb9230fc091617fbf85f
Yang shu mei <pbs71129@gmail.com>
2023-01-02 23:50:28 +0800

    練習五：新增資料

---
+++ app/Http/Controllers/AdminPostsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class AdminPostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'DESC')->get();
+        $data = ['posts' => $posts];
+        return view('admin.posts.index', $data);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create($request->all());
+        return redirect()->route('admin.posts.index');
+    }
+
+    public function edit($id)
+    {
+        return view('admin.posts.edit');
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.master')
+
+@section('page-title', 'Create article')
+
+@section('page-content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">文章管理</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">新增文章</li>
+    </ol>
+    <div class="alert alert-danger alert-dismissible" role="alert" id="liveAlert">
+        <strong>錯誤！</strong> 請修正以下問題：
+        <ul>
+            <li>錯誤 1</li>
+        </ul>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <form action="{{route('admin.posts.store')}}" method="POST" role="form">
+        @csrf
+        <div class="form-group">
+            <label for="title" class="form-label">標題：</label>
+            <input id="title" name="title" class="form-control" value="{{old('title')}}" placeholder="請輸入文章標題">
+
+            <label for="content" class="form-label">內容：</label>
+            <textarea id="content" name="content" class="form-control" rows="10">{{old('content')}}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="is_feature" class="form-label">精選？</label>
+            <select id="is_feature" name="is_feature" class="form-control">
+                <option value="0">否</option>
+                <option value="1">是</option>
+            </select>
+        </div>
+        <div class="text-right">
+            <button class="btn btn-primary btn-sm" type="submit">儲存</button>
+        </div>
+    </form>
+</div>
+@endsection
